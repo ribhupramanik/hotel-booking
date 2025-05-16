06fb8ed856e596c3c0ea9e2de96fac4bcce17569
ribhupramanik <ribhu02@gmail.com>
2025-05-17 00:38:45 +0530

    User Role can be selected during registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'confirmed'],
+            'role' => ['required', 'string', 'in:admin,client'],
         ]);
     }
 
@@ -67,7 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'type' => 'client',
+            'type' => $data['role'],
         ]);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,6 +48,25 @@
                             </div>
 
                             <div class="row mb-3">
+                                <label for="role" class="col-md-4 col-form-label text-md-end">{{ __('Role') }}</label>
+
+                                <div class="col-md-6">
+                                    <select id="role" name="role" class="form-control @error('role') is-invalid @enderror"
+                                        required>
+                                        <option value="" disabled selected>-- Select Role --</option>
+                                        <option value="admin" {{ old('role') == 'admin' ? 'selected' : '' }}>Admin</option>
+                                        <option value="client" {{ old('role') == 'client' ? 'selected' : '' }}>Client</option>
+                                    </select>
+
+                                    @error('role')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
                                 <label for="password"
                                     class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
